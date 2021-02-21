33a8293ecc4aa4142c154cbb70d2ff0ce37c59d1
haeryung <61223256+haeryung96@users.noreply.github.com>
2021-02-21 17:45:52 +0900

    Greedy

--- Algorithm/Greedy/bolling.java
+++ Algorithm/Greedy/bolling.java
@@ -19,7 +19,7 @@ public class bolling {
 		
 		int cnt = 0;
 		
-		for(int i=0; i<=n-1; i++) { // 조합읍 구하려면 입력이 5라면 4만큼 돌리기
+		for(int i=0; i<=n-1; i++) { // 조합을 구하려면 입력이 5라면 4만큼 돌리기
 			for(int j=i+1; j<n; j++) {
 				if(list.get(i)!=list.get(j)) { // 두개의 값이 같지 않다면 다른것이기 때문에 카운트해주기
 					cnt++;
