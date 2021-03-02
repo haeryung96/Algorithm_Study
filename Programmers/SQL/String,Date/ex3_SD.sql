ebccfcd9a8a3348463e41b01857017ce47765047
haeryung <61223256+haeryung96@users.noreply.github.com>
2021-03-02 20:45:58 +0900

    String, Date

---
+++ Programmers/SQL/String,Date/ex3_SD.sql
@@ -0,0 +1,10 @@
+/*
+중성화 여부 파악하기
+*/
+
+SELECT ANIMAL_ID, NAME, 
+(CASE WHEN SEX_UPON_INTAKE LIKE 'Neutered%' THEN 'O'
+      WHEN SEX_UPON_INTAKE LIKE 'Spayed%' THEN 'O'
+      ELSE 'X'
+      END) AS '중성화'
+FROM ANIMAL_INS
