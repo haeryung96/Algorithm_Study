b71a745c5b9544813d46711d4703376fea2b53a2
haeryung <61223256+haeryung96@users.noreply.github.com>
2021-07-17 23:35:03 +0900

    N과 M (11) Backtracking

---
+++ BaekJoon/ex15665.java
@@ -0,0 +1,54 @@
+package baekjoon;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ex15665 {
+	public static int N, M;
+	public static int[] list;
+	public static int[] inputArr;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void DFS(int digit, int input[]) {
+		if(digit == M) {
+			for(int i=0; i < M; i++) {
+				sb.append(list[i]).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		int exNum = 0;
+		for(int i=0; i < N; i++) {
+			if(exNum != input[i]) {
+				exNum = inputArr[i];
+				list[digit] = inputArr[i];
+				DFS(digit+1,input); 
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String [] input = br.readLine().split(" ");
+		
+		N = Integer.parseInt(input[0]);
+		M = Integer.parseInt(input[1]);
+		
+		list = new int[M];
+		inputArr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i < inputArr.length; i++) {
+			inputArr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(inputArr);
+		
+		DFS(0, inputArr);
+		System.out.println(sb);
+	}
+
+}
