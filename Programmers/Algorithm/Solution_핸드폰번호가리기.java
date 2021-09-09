231ef54b2ed5886991a11f9c6d9725157b3492ec
haeryung <61223256+haeryung96@users.noreply.github.com>
2021-09-09 16:36:11 +0900

    Create Solution_핸드폰번호가리기.java

---
+++ Programmers/Algorithm/Solution_핸드폰번호가리기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        
+        for(int i = 0; i < phone_number.length() - 4; i++) { // 11 
+            answer += "*";
+        }
+        
+        for(int i = phone_number.length() - 4; i < phone_number.length(); i++){
+            answer += phone_number.charAt(i);
+        }
+        return answer;
+    }
+}
