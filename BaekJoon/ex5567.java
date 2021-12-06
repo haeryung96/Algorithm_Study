fcf70fedf919c7138bb074f2c18f2118c1161f90
haeryung <61223256+haeryung96@users.noreply.github.com>
2021-12-06 22:56:47 +0900

    결혼식 Graph

--- BaekJoon/ex5567.java
+++ BaekJoon/ex5567.java
@@ -1,63 +1,56 @@
+package BOJ;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
+// 상근이는 1번
 public class ex5567 {
-	public static int N, M;
-	public static int cnt;
-	public static int[][] map;
-	public static boolean[][] visited;
-	public static Queue<Integer> qx = new LinkedList<>();
-	public static Queue<Integer> qy = new LinkedList<>();
-	
-	public static void BFS(int x, int y) {
-		qx.add(x);
-		qy.add(y);
-		visited[x][y] = true;
-		cnt = 0;
-		
-		while(!qx.isEmpty() && !qy.isEmpty()) {
-			int lx = qx.poll();
-			int ly = qy.poll();
-			
-			for(int i=1; i < N; i++) {
-				if(map[lx][i] == 1 && visited[x][i] == false) {
-					
-				}
-			}
-		}
-	}
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine()); // 상근이의 동기의 수 
-		M = Integer.parseInt(br.readLine()); // 리스트의 길이 
-		
-		map = new int[N][N];
-		visited = new boolean[N][N];
-		
-		StringTokenizer st;
-		
-		// 동기의 수만큼 map 채우기
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				map[i][j] = 0;
-			}
-		}
-		
-		// 친구의 관계 
-		for(int i=0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int p1 = Integer.parseInt(st.nextToken());
-			int p2 = Integer.parseInt(st.nextToken());
-			
-			map[p1][p2] = map[p2][p1] = 1;
-		}
-		
-		
-	}
+    public static int N, M;
+    public static int[][] map;
+    public static boolean[] visited;
+    public static Set<Integer> set = new HashSet<>();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine()); // 상근이의 동기의 수
+        M = Integer.parseInt(br.readLine()); // 리스트의 길이
+
+        StringTokenizer st;
+        map = new int[N+1][N+1];
+        visited = new boolean[N+1];
+
+        for(int i=0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int p1 = Integer.parseInt(st.nextToken());
+            int p2 = Integer.parseInt(st.nextToken());
+
+            // 양방향
+            map[p1][p2] = map[p2][p1] = 1;
+        }
+
+        for(int i=2; i < N+1; i++) {
+
+            if(map[1][i] == 1) { // 상근이와 친구인 노드라면
+                if(!visited[i]) { // 친구인데 검사를 하지않았다면
+                    set.add(i); // 노드를 넣고
+                    visited[i] = true;
+                }
+
+                for(int j=2; j < N+1; j++) {
+                    // i번의 친구를 찾아서 초대
+                    if(map[i][j] == 1 &&  !visited[j]) {
+                        set.add(j);
+                        visited[j] = true;
+                    }
+                }
+            }
+        }
+
+        System.out.println(set.size());
+
+    }
 
 }
